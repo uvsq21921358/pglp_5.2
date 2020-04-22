ce786bae5c5bfc129c97b57c9d7f8b265e71c096
SIMPORE <nayimasimpore@gmail.com>
2020-04-22 17:19:32 +0200

    Configuration du fichier Pom

--- src/test/java/uvsq21921358/JDBCTest.java
+++ src/test/java/uvsq21921358/JDBCTest.java
@@ -1,5 +1,25 @@
 package uvsq21921358;
 
-public class JDBCTest {
+import org.junit.Before;
+import org.junit.Test;
 
+public class JDBCTest {
+	
+	private PersonnelImuable personn;
+	private PersonnelGroupe personngroup;
+
+	@Before()
+	public void init() {
+		personn = new PersonnelImuable.Builder("KI","SERGE").dateNaiss(1992,03,18).numT(new NumTelephone("70110203","64567890","5345676788")).build();
+		personngroup = new PersonnelGroupe();
+		PersonnelGroupe group = new PersonnelGroupe();
+		personngroup.addPersonnel(group);
+		personngroup.addPersonnel(personn);
+	}
+	
+	@Test()
+	public void createT() {
+		
+	}
+	
 }
